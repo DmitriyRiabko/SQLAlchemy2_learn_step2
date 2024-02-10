a8fce799bbeab3a30e636628ab0c6b69ebb40e1c
vsegda_vinovat <riabko97@gmail.com>
2024-02-10 21:27:50 +0200

    added new custom types

--- src/database.py
+++ src/database.py
@@ -1,6 +1,11 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession #async connections
-from sqlalchemy.orm import Session, sessionmaker, DeclarativeBase#sync connection
-from sqlalchemy import create_engine, text, URL
+from typing import Annotated
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    async_sessionmaker,
+    AsyncSession,
+)  # async connections
+from sqlalchemy.orm import Session, sessionmaker, DeclarativeBase  # sync connection
+from sqlalchemy import String, create_engine, text, URL
 
 from config import settings
 
@@ -9,19 +14,23 @@ sync_engine = create_engine(
     echo=True,
     # pool_size=5,
     # max_overflow=10,
-    
 )
 async_engine = create_async_engine(
     url=settings.DATABASE_URL_psycopg,
     echo=True,
     # pool_size=5,
     # max_overflow=10,
-    
 )
 
 
 session_factory = sessionmaker(sync_engine)
 async_session_factory = async_sessionmaker(async_engine)
 
+
+str_256 = Annotated[str, 256]
+
+
 class Base(DeclarativeBase):
-    ...
+    type_annotation_map = {
+        str_256: String(256)
+        }
