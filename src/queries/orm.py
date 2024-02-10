83084274cb197480b39070d34e820059b45efba1
vsegda_vinovat <riabko97@gmail.com>
2024-02-10 21:27:02 +0200

    orm mode

--- src/queries/orm.py
+++ src/queries/orm.py
@@ -1,22 +1,39 @@
-from models import metadat_obj,WorkersOrm
-from database import sync_engine,session_factory
+from models import WorkersOrm
+from database import sync_engine,session_factory, Base
+from sqlalchemy import select, update
 
 
 
 
+class SyncOrm:
 
-
-def create_tables():
-    sync_engine.echo = False
-    metadat_obj.drop_all(bind=sync_engine)
-    metadat_obj.create_all(bind=sync_engine)
-    sync_engine.echo = True
-    
-    
-def insert_data():
-    with session_factory() as session:
-        worker_bobr = WorkersOrm(username='bobr')
-        worker_zhmix = WorkersOrm(username='zhmix')
+    @staticmethod
+    def create_tables():
+        sync_engine.echo = False
+        Base.metadata.drop_all(bind=sync_engine)
+        Base.metadata.create_all(bind=sync_engine)
+        sync_engine.echo = True
         
-        session.add_all([worker_bobr,worker_zhmix])
-        session.commit()    
+        
+    @staticmethod
+    def insert_data():
+        with session_factory() as session:
+            worker_bobr = WorkersOrm(username='bobr')
+            worker_zhmix = WorkersOrm(username='zhmix')
+            
+            session.add_all([worker_bobr,worker_zhmix])
+            session.commit()    
+            
+            
+            
+    @staticmethod
+    def select_workers():
+        with sync_engine.connect() as conn:
+            ...
+          
+            
+            
+    @staticmethod
+    def update_workers(new_username:str ='Pcheluk',worker_id:int=2):
+        with sync_engine.connect() as conn:
+          ...
